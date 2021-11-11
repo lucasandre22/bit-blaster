937a98dd7be368c6ba29384da948afb263c93c0b
Lucas <lucas.andre22@yahoo.com>
2021-11-11 19:07:16 -0300

    Added entity factory

---
+++ bit-blaster/src/main/java/com/bitblaster/utils/EntityFactory.java
@@ -0,0 +1,15 @@
+package com.bitblaster.utils;
+
+import com.bitblaster.entity.Enemy;
+import com.bitblaster.entity.Player;
+
+public class EntityFactory {
+
+	public static Player createPlayer(int x, int y, int height, int width, String imagePath) {
+		return new Player(new Vector2D<Integer>(x,y), new Vector2D<Integer>(height,width), imagePath);
+	}
+
+	public static Enemy createEnemy(int x, int y, int height, int width, String imagePath) {
+		return new Enemy(new Vector2D<Integer>(x,y), new Vector2D<Integer>(height, width), imagePath));
+	}
+}
